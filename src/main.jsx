81923645e15d6bac8ec1a761e86f20f956e3f608
MAXSUEL RODRIGUES SANTOS <maxsuelrodriguessantos15@gmail.com>
2026-02-27 22:46:43 -0300

    aula 27/02/26

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+
+import ReactDom from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+
+import './estilos/reset.css'
+import './estilos/globals.css'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+const root = ReactDom.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
     <App />
-  </StrictMode>,
-)
+    </BrowserRouter>
+  </React.StrictMode>,
+);
---
+++ src/pages/sobre/index.jsx
@@ -0,0 +1,9 @@
+
+export default function Sobre() {
+  return (
+    <>
+      <h1>Sobre</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia sit, quo accusamus minus odio quidem iure, impedit, exercitationem tenetur ab quaerat delectus provident eaque quia? Praesentium molestiae temporibus animi nostrum.</p>
+    </>
+  )
+}
